42018047b69fe23142a671b5c470a3cc3d4ebac4
Steven Job <sjob@scottlogic.co.uk>
2016-08-29 07:52:32 +0100

    Add value to in memory Repository

--- shared/src/main/java/com/scuttler/vegetable/shared/InMemoryVegetableRepository.java
+++ shared/src/main/java/com/scuttler/vegetable/shared/InMemoryVegetableRepository.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Repository
+@Repository("InMemVeg")
 public class InMemoryVegetableRepository implements VegetableRepository {
 
     Map<String, Vegetable> vegetables = new HashMap<>();
